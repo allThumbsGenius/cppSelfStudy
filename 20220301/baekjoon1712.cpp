4b21ef73b48089f6faa2c82915498b421140cb17
allThumbsGenius <butijung@naver.com>
2022-03-01 13:34:36 +0900

    1.2v

--- 20220301/baekjoon1712.cpp
+++ 20220301/baekjoon1712.cpp
@@ -18,6 +18,11 @@ int main(){
 
 int getBreakEvenPoint(int fixedCost, int variableCost, int price){
 	int breakEvenPoint;
+
+	if(price - variableCost == 0){
+		return -1;
+	}
+
 	breakEvenPoint = (fixedCost) / (price - variableCost) + 1;
 	if(breakEvenPoint < 0){
 		return -1;
