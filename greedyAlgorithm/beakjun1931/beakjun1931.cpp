40896ade6731718ecbc120d346cad6d4de46a66a
allThumbsGenius <butijung@naver.com>
2022-05-14 22:55:04 +0900

    1.0v

---
+++ greedyAlgorithm/beakjun1931/beakjun1931.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+class Meeting{
+	public:
+		int startingTime;
+		int finishingTime;
+
+		Meeting(int _startingTime, int _finishingTime)
+			:startingTime(_startingTime), finishingTime(_finishingTime){
+		}
+};
+
+bool compareByFinishingTime(const Meeting &a, const Meeting &b){
+	return a.finishingTime < b.finishingTime;
+}
+
+
+bool compareByStartingTime(const Meeting &a, const Meeting &b){
+	return a.startingTime < b.startingTime;
+}
+
+int main(){
+	int n;
+	std::vector<Meeting> meetings;
+	int count = 1;
+
+	std::cin >> n;
+
+	for(int i = 0; i < n; ++i){
+		int a, b;
+		std::cin >> a >> b;
+		meetings.push_back(Meeting(a, b));
+	}
+
+	std::sort(meetings.begin(), meetings.end(), compareByStartingTime);
+	std::sort(meetings.begin(), meetings.end(), compareByFinishingTime);
+
+	int curruntFinishingTime = meetings[0].finishingTime;
+	
+	for(int i = 0; i < n; ++i){
+		if(curruntFinishingTime <= meetings[i].startingTime){
+			curruntFinishingTime = meetings[i].finishingTime;
+			++count;
+		}
+	}
+
+	std::cout << count;
+
+	return 0;
+}
+
+
